e8346af7964ec89a7ea89cd06db40764f88cfd3e
Daniel <sanjose1994@gmail.com>
2019-05-15 16:51:41 -0700

    updating pc with current changes

--- models/concept.js
+++ models/concept.js
@@ -3,10 +3,31 @@ const Schema = mongoose.Schema;
 
 const titleOne = new Schema({
 
-  title: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  passwordConf: {
+    type: String,
+    required: true,
+  },
+  concept: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
 
-   });
+  }
+
+});
 
 
 const Concept = mongoose.model("Concept", titleOne);
