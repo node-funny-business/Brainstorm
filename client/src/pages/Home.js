e60506596cc8147311ad52fedac278a32eeafbff
jason <jasonkellogglee@gmail.com>
2019-05-15 22:13:41 -0700

    added grid for cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,30 @@
 import React from "react";
 import Nav from "../components/Nav"
 import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
 
 function Home() {
     return (
-    <div>
-        <Nav />
-        <Card>
-            <p>Hello</p>
-        </Card>
-        <Card>
-            <p>Wsup</p>
-        </Card>
-        <Card>
-            <p>Yee!</p>
-        </Card>
-    </div>
+        <div>
+            <Nav />
+            <Grid container spacing={24}>
+                <Grid item xs={4}>
+                    <Card>
+                        <p>Hello</p>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                    <Card>
+                        <p>Wsup</p>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                    <Card>
+                        <p>Yee!</p>
+                    </Card>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
