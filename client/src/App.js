7bb7442495053fbb500fdbfc5a603be8ec0fdaf7
Daniel <sanjose1994@gmail.com>
2019-05-20 13:56:00 -0700

    added the okta to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
 // import API from "./utils/API";
@@ -8,7 +8,7 @@ import Instructions from "./pages/Instructions"
 import MyAccount from "./pages/MyAccount"
 import Home from "./pages/Home";
 import Login from "./components/auth/login";
-import { Switch } from "@material-ui/core";
+// import { Switch } from "@material-ui/core";
 
 function onAuthRequired({ history }) {
   history.push('/login');
@@ -16,76 +16,24 @@ function onAuthRequired({ history }) {
 
 
 class App extends Component {
-
-  // state = {
-  //   text: "",
-  //   results: []
-  // }
-
-  // componentWillMount() {
-  //   this.getAll();
-  // }
-
-
-  // handleFormSubmit = event => {
-  //   event.preventDefault();
-  //   this.saveConcept(`${this.state.text}`)
-  //   this.getAll();
-  //   // alert(`Text: ${this.state.text}`)
-  // }
-
-  // saveConcept = query => {
-  //   API.saveConcept(query)
-  //     .then(res => console.log(res))
-  //     .catch(err => console.log(err));
-  // }
-
-  // handleInputChange = key => event => {
-  //   const value = event.target.value;
-  //   this.setState({ 
-  //     [key]: value
-  //   });
-  // };
-
-  // getAll = () => {
-  //   API.getConcepts()
-  //     .then(res => this.setState({ results: res.data }))
-  //     .catch(err => console.log(err));
-  // }
-
   render() {
     return (
-      // <div>
-      //   <h1>Topic</h1>
-      //   <p>Get text: {JSON.stringify(this.state.results)}</p>
-      //   <form className="form" onSubmit={this.handleFormSubmit}>
-      //     <input
-      //       value={this.state.text}
-      //       onChange={this.handleInputChange}
-      //       type="text"
-      //       placeholder="Concept 1"
-      //     />
-      //   </form>
-      // </div>
       <Router>
-
         <Security issuer='https://dev-363275.okta.com/oauth2/default'
-          client_id='0oam45rndnEWAIpaA356'
-          redirect_uri={window.location.origin + '/implicit/callback'}
-          onAuthRequired={onAuthRequired} >
-          <div className="App">
-            <Route exact={true} path='/' component={Home} />
-            <SecureRoute exact={true} path='/aboutus' component={AboutUs} />
-            <Route exact path='/instructions' component={Instructions} />
-            <Route exact path='/myaccount' component={MyAccount} />
-            <Route path='/login' render={() => <Login baseUrl='https://dev-363275.okta.com' />} />
-            <Route path='/implicit/callback' component={ImplicitCallback} />
-          </div>
+                  client_id='0oam45rndnEWAIpaA356'
+                  redirect_uri={window.location.origin + '/implicit/callback'}
+                  onAuthRequired={onAuthRequired} >
+          
+          <Route path='/' exact={true} component={Instructions} />
+          <SecureRoute path='/home' exact={true} component={Home} />
+          <Route path='/myaccount' exact={true} component={MyAccount} />
+          <Route path='/aboutus' exact={true} component={AboutUs} />
+          <Route path='/login' render={() => <Login baseUrl='https://dev-363275.okta.com' />} />
+          <Route path='/implicit/callback' component={ImplicitCallback} />
         </Security>
       </Router>
-
-    )
-  };
+    );
+  }
 }
 
 export default App;
