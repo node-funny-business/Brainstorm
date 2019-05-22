4395f442c3a6d8efb004520d04c7d6acecc5a15f
Jonathan Viera <go_chunkymonkey@yahoo.com>
2019-05-22 10:13:16 -0700

    pushing changes to brainstorm model

--- models/brainstorm.js
+++ models/brainstorm.js
@@ -1,16 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
     var Brainstorm = sequelize.define('Brainstorm', {
-        id: 10,
-        parent_id: null,
-        text_type: 'Brainstorm',
         name: {
             type: DataTypes.String,
             allowNull: false,
             validate: {
                 len: [1]
             }
-        },
-        
+        }
     });
     return Brainstorm;
 };
