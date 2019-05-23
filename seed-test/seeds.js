997d2c8640803e279a45eb385da1e2ea58d0ccea
Daniel <sanjose1994@gmail.com>
2019-05-23 09:30:53 -0700

    merging to goon2

--- seed-test/seeds.js
+++ seed-test/seeds.js
@@ -2,17 +2,29 @@ var db = require("../controllers/db")
 
 var seeds = {
     Brainstorm: [{
-    name: "pizza",
+    name: "pizza"
 },{
     name: "Pie"
 },{
     name: "Sandwhich"
 }], Concept: [{
     concept: "hello boys"
+},{
+    concept: "meh"
+},{
+    concept: "bleh"
 }], Idea: [{
     idea: "hey girl hey"
+},{
+    idea: "hey"
+},{
+    idea: "ey"
 }], Steps: [{
     steps: "dsjaklfl"
+},{
+    steps: "fioejfi"
+},{
+    steps: "fewjhfewhfoiwh"
 }]};
 db.sequelize.sync({ force: true }).then(function () {
     for (var key in seeds) {
